4ec633f80dcd0233e94d8b3810ce96b0a4d1beb1
Jonathon Nguyen <nguyjd@gmail.com>
2021-12-28 13:26:00 -0500

    Created scripts for ros

---
+++ ros_ws/build_package.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt install python3-colcon-common-extensions
+
+colcon build
---
+++ ros_ws/run_ros_package.sh
@@ -0,0 +1,3 @@
+source /opt/ros/foxy/setup.bash
+source install/setup.bash
+ros2 launch mowbot mowbot.launch.py
